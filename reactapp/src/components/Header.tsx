505994b2819d484ad27340d7d16aaac2bedadb87
Not0range <vanorr2000@gmail.com>
2023-06-30 12:20:15 +0300

    Change logo

--- reactapp/src/components/Header.tsx
+++ reactapp/src/components/Header.tsx
@@ -3,7 +3,7 @@ import '../styles/Header.css';
 export default function Header() {
     return (
         <div className='main-header'>
-            <img src="/logo192.png" alt='' />
+            <img src="/logo.png" alt='' />
             <div style={{ flexGrow: 1 }} />
             <a href='/' target='_blank'><i className='bx bxl-facebook bx-sm' /></a>
             <a href='/' target='_blank'><i className='bx bxl-instagram bx-sm' /></a>
