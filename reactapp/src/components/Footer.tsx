403d86579a029403f16ab542bdef39b0fdeff5aa
Not0range <vanorr2000@gmail.com>
2023-06-30 12:18:49 +0300

    Add footer

--- reactapp/src/components/Footer.tsx
+++ reactapp/src/components/Footer.tsx
@@ -1,6 +1,18 @@
+import '../styles/Footer.css';
+
 export default function Footer() {
     return (
-        <div>
-        </div>
+        <footer>
+            <div>
+                <div className='footer-section1'>
+                    <h2> Поделитесь информацией в социальных сетях </h2>
+                    <h4> О коронавирусе и методах защиты от него должно узнать как можно больше людей. </h4>
+                </div>
+                <div className='footer-section2'>
+                    <h4> Телефон горячей линии: </h4>
+                    <h2> 0(533) 9-44-88 </h2>
+                </div>
+            </div>
+        </footer>
     )
 }
